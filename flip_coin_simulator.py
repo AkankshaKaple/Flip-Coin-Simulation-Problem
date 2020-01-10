a47be873c91228cdc35186c917094c9e01638444
AkankshaKaple <akankshakaple@gmail.com>
2020-01-10 18:48:09 +0530

    Flip coin, store result in dict and get percentage of occurance of head and tail

--- flip_coin_simulator.py
+++ flip_coin_simulator.py
@@ -1,19 +1,21 @@
 import random
 
 
-class FlipCoinSimulator:
-    def flip_coin(self):
+def flip_coin():
+    tail = []
+    head = []
+    for i in range(num_chances):
         if random.randrange(0, 2) == 0:
-            side = "T"
+            tail.append("T")
         else:
-            side = "H"
-        return side
+            head.append("H")
+    return {"tail": tail, "head": head}
 
 
 print("Welcome to Flip coin simulator")
-fc = FlipCoinSimulator()
-output_side = fc.flip_coin()
-print(output_side)
-
-
-
+num_chances = 10
+result = flip_coin()
+print(result)
+tail_percentage = (result['tail'].count("T") * num_chances) / 100
+head_percentage = (result['head'].count("H") * num_chances) / 100
+print("head_percentage = {} % {}tail_percentage = {} % ".format(head_percentage, '\n', tail_percentage))

920ccc341c80acbc2c39e912cf4a5cdd3fca0282
AkankshaKaple <akankshakaple@gmail.com>
2020-01-10 18:36:27 +0530

    Added: Program to simulated coin fliping

--- flip_coin_simulator.py
+++ flip_coin_simulator.py
@@ -1,1 +1,19 @@
+import random
+
+
+class FlipCoinSimulator:
+    def flip_coin(self):
+        if random.randrange(0, 2) == 0:
+            side = "T"
+        else:
+            side = "H"
+        return side
+
+
 print("Welcome to Flip coin simulator")
+fc = FlipCoinSimulator()
+output_side = fc.flip_coin()
+print(output_side)
+
+
+
